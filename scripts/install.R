dbc86ca287a4d0c25f29c7c20ff54588fba545b7
Satoyoshi_ISHIZAKI <ishizaki-satoyoshi766@g.ecc.u-tokyo.ac.jp>
2026-03-20 17:00:54 +0900

    Started analysis.

--- scripts/install.R
+++ scripts/install.R
@@ -1,69 +1,65 @@
 #install packages needed (not via "install.packages")
 
 install.packages("devtools")
+install.packages("BiocManager")
 install.packages("readxl")
 install.packages("tidyverse")
 install.packages("ggfortify")
 install.packages("ggeffects")
 install.packages("ggnewscale")
 install.packages("vegan")
-install.packages("ggtern")
 install.packages("dichromat")
-install.packages("compositions")
-install.packages("robCompositions")
-devtools::install_github("pmartinezarbizu/pairwiseAdonis/pairwiseAdonis")
-install.packages("picante")
-install.packages("glm2")
 
-##Bioconductor
+## Bioconductor
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install(version = "3.19")
 
-##phyloseq
+## phyloseq
 if(!requireNamespace("BiocManager")){
   install.packages("BiocManager")
   }
 BiocManager::install("phyloseq")
 
-##qiime2R
+## qiime2R
 if (!requireNamespace("devtools", quietly = TRUE)){install.packages("devtools")}
 devtools::install_github("jbisanz/qiime2R")
 
 
 
-###yet to be done
-##fantaxtic
+### yet to be done
+
+# ANOCOM-BC
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("ANCOMBC")
+
+# microbiome
+## needed for ANCOM-BC
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("microbiome")
+
+## fantaxtic
 if(!"devtools" %in% installed.packages()){
   install.packages("devtools")
 }
 devtools::install_github("gmteunisse/fantaxtic")
 
-##ggnested
+## ggnested
 if(!"devtools" %in% installed.packages()){
   install.packages("devtools")
 }
 devtools::install_github("gmteunisse/ggnested")
 
-##biomformat
+## biomformat
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("biomformat")
 
-#XLconnect
+# XLconnect
 install.packages("XLConnect")
 
-#readGenAlEx
+# readGenAlEx
 install.packages("devtools")
-#devtools::install_github("douglasgscofield/readGenalex")
-
-#ANOCOM-BC
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("ANCOMBC")
-
-#microbiome
-##needed for ANCOM-BC
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("microbiome")
+# devtools::install_github("douglasgscofield/readGenalex")
